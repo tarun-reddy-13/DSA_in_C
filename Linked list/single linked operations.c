dbc3079a329c5fc81fdbc12344d33d2838994d61
tarun-reddy-13 <96988906+tarun-reddy-13@users.noreply.github.com>
2022-11-23 17:32:26 +0530

    Update single linked operations.c

--- Linked list/single linked operations.c
+++ Linked list/single linked operations.c
@@ -276,7 +276,7 @@ int main()
     			break;
 		}
 		printf("\n");
-		printf("\nEnter your choice (a,b,c,d,e,f,g,h,i): ");
+		printf("\nEnter your choice (0,1,2,3,4,5,6,7,8,9): ");
     	scanf("%d",&choice);
 	}
 	return 0;
